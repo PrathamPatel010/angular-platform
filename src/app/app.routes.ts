f52d5a8dc5ee90039801141110dfd2996ceedd2e
Pratham Patel <ppratham812@gmail.com>
2026-01-11 00:22:54 +0530

    developed basic app layout shell for testing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { LayoutComponent } from "./layout/layout.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    component: LayoutComponent,
+    children: [
+      {
+        path: "",
+        redirectTo: "home",
+        pathMatch: "full",
+      },
+      {
+        path: "home",
+        loadComponent: () =>
+          import("./features/home/home.component").then((c) => c.HomeComponent),
+      },
+    ],
+  },
+];
